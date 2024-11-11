857c4606292b9c69837fcdeb5414fe96ed537f9b
Rohithhowk <rohith28chowki@gmail.com>
2024-11-11 13:38:39 +0530

    solved two qns passing all test cases

--- program1.py
+++ program1.py
@@ -1,6 +1,22 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        def exploreIsland(row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 'W':
+                return
+            
+            grid[row][col] = 'W'
+            
+            exploreIsland(row - 1, col)  
+            exploreIsland(row + 1, col)  
+            exploreIsland(row, col - 1)  
+            exploreIsland(row, col + 1)  
+        
+        island_count = 0
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 'L':
+                    island_count += 1
+                    exploreIsland(r, c)
+        
+        return island_count
--- program2.py
+++ program2.py
@@ -1,5 +1,25 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    s_index = 0
+    p_index = 0
+    star_index = -1
+    match_index = -1
 
-# write your code here
-  
-        return False
+    while s_index < len(s):
+        if p_index < len(p) and (p[p_index] == s[s_index] or p[p_index] == '?'):
+            s_index += 1
+            p_index += 1
+        elif p_index < len(p) and p[p_index] == '*':
+            star_index = p_index
+            match_index = s_index
+            p_index += 1
+        elif star_index != -1:
+            p_index = star_index + 1
+            match_index += 1
+            s_index = match_index
+        else:
+            return False
+
+    while p_index < len(p) and p[p_index] == '*':
+        p_index += 1
+
+    return p_index == len(p)
